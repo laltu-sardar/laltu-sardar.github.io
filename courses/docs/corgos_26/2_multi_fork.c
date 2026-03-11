5b2233936024589d4b521dd994101da3dc05f2b6
Laltu Sardar <102033315+laltu-sardar@users.noreply.github.com>
2026-03-11 12:10:55 +0530

    Add files via upload

---
+++ courses/docs/corgos_26/2_multi_fork.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+    /* fork a child process */
+    fork();
+    /* fork another child process */
+    fork();
+    /* and fork another */
+    fork();
+    printf("A");
+    return 0;
+}
+
